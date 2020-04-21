05fd6844fba4c0574b4bdfd2f0bc0c00109afb16
Ethan Pappion <ethanpappion@pop-os.localdomain>
2020-04-20 22:33:49 -0500

    added styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <div class="">
-      <input v-on:keyup.capture="Search('3166-2:UM'), Details" v-model="input" placeholder="Search Movies, TV, Film..">
+<div id="app">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <!-- Slide One - Set the background image for this slide in the line below -->
+        <div class="carousel-item active" >
+          <div class="carousel-caption d-none d-md-block">
+            <h2 class="display-4">First Slide</h2>
+            <p class="lead">This is a description for the first slide.</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <Results v-if="searchData != null" v-bind:searchData = "searchData"/>
+  </header>
+  <div class="container">
+    <br />
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-10 col-lg-8">
+        <form class="card card-sm">
+          <div class="card-body row no-gutters align-items-center">
+            <div class="col-auto">
+              <i class="fas fa-search h4 text-body"></i>
+            </div>
+            <!--end of col-->
+            <div class="col">
+              <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search any movie or TV show.. " v-on:keyup.capture="Search('3166-2:UM'), Details" v-model="input">
+            </div>
+            <!--end of col-->
+          </div>
+        </form>
+      </div>
+      <!--end of col-->
+    </div>
   </div>
+  <Results v-if="searchData" v-bind:searchData="searchData" v-bind:searchDetails="searchDetails"/>
+</div>
 </template>
 
 <script>
@@ -35,47 +76,47 @@ export default {
         return null;
       } else {
         return fetch(url)
-        .then((response) => response.json())
-        .then((responseData) => {
-          this.searchData = responseData;
-          //console.log(this.searchData);
-          //console.log(url);
-        })
-        .catch(error => console.warn(error));
+          .then((response) => response.json())
+          .then((responseData) => {
+            this.searchData = responseData;
+            //console.log(this.searchData);
+            //console.log(url);
+          })
+          .catch(error => console.warn(error));
       }
     },
     TvDetails: function tvDetails(ID) {
       let url = ''.concat(baseURL, 'tv/', ID, '?api_key=', APIKEY);
       //console.log(url);
       return fetch(url)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.searchDetails = responseData;
-        //console.log(this.searchDetails);
-      })
-      .catch(error => console.warn(error));
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.searchDetails = responseData;
+          console.log(this.searchDetails);
+        })
+        .catch(error => console.warn(error));
     },
     MovieDetails: function movieDetails(ID) {
       let url = ''.concat(baseURL, 'movie/', ID, '?api_key=', APIKEY);
       //console.log(url);
       return fetch(url)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.searchDetails = responseData;
-        //console.log(this.searchDetails);
-      })
-      .catch(error => console.warn(error));
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.searchDetails = responseData;
+          console.log(this.searchDetails);
+        })
+        .catch(error => console.warn(error));
     },
     StreamDetails: function streamTVDetails(id) {
       let url = ''.concat(streamURL, 'tv/', id, '/watch');
-      console.log(url);
+      //console.log(url);
       return fetch(url)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.streamDetails = responseData;
-        console.log(this.streamDetails);
-      })
-      .catch(error => console.warn(error));
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.streamDetails = responseData;
+          //console.log(this.streamDetails);
+        })
+        .catch(error => console.warn(error));
     },
   },
   mounted() {
@@ -89,9 +130,9 @@ export default {
         this.searchDetails == "";
         return this.searchDetails;
       } else if (this.searchData.results[0].media_type == 'tv') {
-        this.StreamDetails(showID);
+        this.TvDetails(showID);
         return this.searchDetails;
-      } else if (this.searchData.results[0].media_type == 'movie'){
+      } else if (this.searchData.results[0].media_type == 'movie') {
         this.MovieDetails(showID);
         return this.searchDetails;
       } else {
@@ -110,5 +151,57 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 80px;
+}
+
+.form-control-borderless {
+  border: none;
+}
+
+.form-control-borderless:hover,
+.form-control-borderless:active,
+.form-control-borderless:focus {
+  border: none;
+  outline: none;
+  box-shadow: none;
+}
+.carousel-item {
+  height: 100vh;
+  min-height: 100px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-image: url('assets/banner.png');
+}
+section {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+}
+
+.lnr {
+  display: inline-block;
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.05em;
+  stroke-width: 1;
+}
+
+.services-icon {
+  margin-bottom: 20px;
+  font-size: 30px;
+}
+bgc2, .vLine, .hLine {
+    background-color: #0F52BA;
+}
+
+.quote-icon {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+.services-icon {
+    font-size: 60px;
+    margin-left: 2rem;
 }
 </style>
--- client/src/components/Results.vue
+++ client/src/components/Results.vue
@@ -1,30 +1,164 @@
 <template>
-  <div id="test" v-if="searchData.results[0]">
-    <p> {{searchData.results[0].name || searchData.results[0].title}} </p>
-    <p> {{ searchData.results[0].overview }} </p>
-    <p> {{ searchData.results[0].first_air_date || searchData.results[0].release_date }}</p>
-
-  </div>
+<div id="test" v-if="searchData.results[0] != null || searchData.results[0] != 'undefined'">
+  <section class="py-5 text-center">
+    <div class="container">
+      <h2 class="text-center"> {{ Name || Title }}</h2>
+      <p class="text-muted mb-5 text-center"> {{ Overview }}</p>
+      <div class="row">
+        <div v-if="FirstAir !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-magic-wand"></use>
+          </svg>
+          <h6>First Aired</h6>
+          <p class="text-muted"> {{FirstAir}} </p>
+        </div>
+        <div v-if="ReleaseDate !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-heart"></use>
+          </svg>
+          <h6>Release Date</h6>
+          <p class="text-muted">{{ReleaseDate}}</p>
+        </div>
+        <div v-if="MediaType !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-rocket"></use>
+          </svg>
+          <h6>Media Type</h6>
+          <p class="text-muted">{{MediaType}}</p>
+        </div>
+        <div v-if="Seasons !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-paperclip"></use>
+          </svg>
+          <h6>Number of Seasons</h6>
+          <p class="text-muted">{{Seasons}}</p>
+        </div>
+        <div v-if="Episodes !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-screen"></use>
+          </svg>
+          <h6>Number of Episodes</h6>
+          <p class="text-muted">{{Episodes}}</p>
+        </div>
+        <div v-if="Revenue !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-inbox"></use>
+          </svg>
+          <h6>Revenue</h6>
+          <p class="text-muted">{{Revenue}}</p>
+        </div>
+        <div v-if="Runtime !=null" class="col-sm-6 col-lg-4 mb-3">
+          <svg class="lnr text-primary services-icon">
+            <use xlink:href="#lnr-inbox"></use>
+          </svg>
+          <h6>Runtime</h6>
+          <p class="text-muted">{{Runtime}}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
 </template>
 
 <script>
-
 export default {
   name: 'Results',
   props: {
     searchData: {
       required: true,
     },
-    streamDetails: {
-
+    searchDetails: {
+      required: true,
     },
   },
   data() {
-    return {
-    }
+    return {}
   },
   computed: {
-
+    Overview: function() {
+      if (this.searchData.results[0] == null || this.searchData.results[0] == "") {
+        return null;
+      } else {
+        return this.searchData.results[0].overview;
+      }
+    },
+    Name: function() {
+      if (this.searchData.results[0] == null || this.searchData.results[0] == "") {
+        return null;
+      } else {
+        return this.searchData.results[0].name;
+      }
+    },
+    Title: function() {
+      if (this.searchData.results[0] == null || this.searchData.results[0] == "") {
+        return null;
+      } else {
+        return this.searchData.results[0].title;
+      }
+    },
+    FirstAir: function() {
+      if (this.searchData.results[0] == null || this.searchData.results[0].first_air_date == "") {
+        return null;
+      } else {
+        return this.searchData.results[0].first_air_date;
+      }
+    },
+    ReleaseDate: function() {
+      if (this.searchData.results[0] == null) {
+        return null;
+      } else {
+        return this.searchData.results[0].release_date;
+      }
+    },
+    MediaType: function() {
+      if (this.searchData.results[0] == null) {
+        return null;
+      } else {
+        return this.searchData.results[0].media_type.toUpperCase();
+      }
+    },
+    Seasons: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return this.searchDetails.number_of_seasons;
+      }
+    },
+    Episodes: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return this.searchDetails.number_of_episodes;
+      }
+    },
+    Revenue: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return "$" + this.searchDetails.revenue.toString();
+      }
+    },
+    Runtime: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return this.searchDetails.runtime.toString() + "minutes";
+      }
+    },
+    Status: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return this.searchDetails.status;
+      }
+    },
+    Budget: function() {
+      if (this.searchDetails == null) {
+        return null;
+      } else {
+        return "$" + this.searchDetails.budget.toString();
+      }
+    },
   },
   methods: {
 
@@ -36,5 +170,9 @@ export default {
 #test {
   text-align: center;
 
+}
+section {
+  padding-top: 5rem;
+  padding-bottom: 5rem;
 }
 </style>
